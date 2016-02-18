42e47410ba60e7c3d734ab26e3d70a2cb454f93d
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-02-18 13:10:06 -0700

    The Jtable is now correctly implemented

--- src/JtableView/ArrayPanel.java
+++ src/JtableView/ArrayPanel.java
@@ -1,52 +1,59 @@
 package JtableView;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.SpringLayout;
+import javax.swing.table.DefaultTableModel;
+
 import JtableController.ArrayController;
 
 public class ArrayPanel extends JPanel
 {
 	private ArrayController baseController;
 	private JTable stringTable;
-	
-	String [] columnNames = {"0","1","2","3"};
-	Object[][] data = {{0,0,0}, {1,1,1}, {2,2,2}, {3,3,3}};
-	
-	public ArrayPanel( ArrayController baseController)
-	{	
-		JTable stringTable = new JTable(data, columnNames);
-		
+	private SpringLayout baseLayout;
+
+	String[] columnNames = { "0", "1", "2", "3" };
+	Object[][] data = { { 0, 0, 0 }, { 1, 1, 1 }, { 2, 2, 2 }, { 3, 3, 3 } };
+
+	public ArrayPanel(ArrayController baseController)
+	{
+		stringTable = new JTable();
+
 		this.baseController = baseController;
-		
+		baseLayout = new SpringLayout();
+
 		setupPanel();
+		setupTable();
 		setupLayout();
 		SetupListeners();
 	}
 
-
-
 	private void setupPanel()
 	{
+		this.setBackground(Color.GREEN);
+
 		this.add(stringTable);
-		
-		
+
 	}
 
+	private void setupTable()
+	{
+		DefaultTableModel myTableModel = new DefaultTableModel(data, columnNames);
+		stringTable.setModel(myTableModel);
+	}
 
 	private void setupLayout()
 	{
-		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	private void SetupListeners()
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 }
