ab209b41efb186072761c4c2adc05ddf4941d086
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-02-23 11:39:18 -0700

    Added my frame reference in controller.

--- src/JtableController/ArrayController.java
+++ src/JtableController/ArrayController.java
@@ -1,7 +1,18 @@
 package JtableController;
 
+import JtableView.JtableFrame;
+
 public class ArrayController 
 {
+	private JtableFrame baseFrame;
+	
+	public ArrayController()
+	{
+		baseFrame = new JtableFrame(this);
+		
+	}
+	
+	
 	public void start()
 	{
 		
